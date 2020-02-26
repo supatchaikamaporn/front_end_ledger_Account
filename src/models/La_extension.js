a4b9ce788e6eef3e038e16226868586407eb93c7
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-02-26 09:35:49 +0700

    1/3 frontend
    1/3 frontend

--- src/models/La_extension.js
+++
@@ -1,7 +0,0 @@
-exports.La_extension = class {
-  constructor(lae_id, lae_descript, lae_timestamp) {
-    this.lae_id = lae_id;
-    this.lae_descript = lae_descript;
-    this.lae_timestamp = lae_timestamp;
-  }
-};
