7292dbc36b652d49bcbe067b738f48c525f089ad
supatchaikamaporn <61458075+supatchaikamaporn@users.noreply.github.com>
2020-03-03 15:07:31 +0700

    Update Account.vue

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -147,6 +147,7 @@ export default {
         .dialog({
           title: "Confirm",
           message: "Really delete",
+          kuy
           cancel: true,
           persisten: true
         })
